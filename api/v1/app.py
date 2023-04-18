50239f0eecf698efa071d73ff27d7e5e05f00495
root <root@LAPTOP-BS5Q7L8B.localdomain>
2023-04-18 15:44:06 -0500

    comments Nikki

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,18 +8,24 @@ from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
+# Initialize the Flask app
 app = Flask(__name__)
+# Set configuration options for Flask
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+# Register the blueprint for the views
 app.register_blueprint(app_views)
+# Enable CORS for the app
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+# Define a funct to close the database conn when the app is done
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
     storage.close()
 
 
+# Define an error handler for 404 errors
 @app.errorhandler(404)
 def not_found(error):
     """ 404 Error
@@ -28,17 +34,23 @@ def not_found(error):
       404:
         description: a resource was not found
     """
+# Define the response to send back for 404 errors
     return make_response(jsonify({'error': "Not found"}), 404)
 
+
+# Set configuration options for the Swagger documentation
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone Restful API',
     'uiversion': 3
 }
 
+# Initialize the Swagger documentation for the app
 Swagger(app)
 
 
+# Start the app if this is the main script being run
 if __name__ == "__main__":
+    # Get the host and port conf. options from env. var, or use defaults
     """ Main Function """
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
@@ -46,4 +58,6 @@ if __name__ == "__main__":
         host = '0.0.0.0'
     if not port:
         port = '5000'
+
+    # Start the Flask app
     app.run(host=host, port=port, threaded=True)
