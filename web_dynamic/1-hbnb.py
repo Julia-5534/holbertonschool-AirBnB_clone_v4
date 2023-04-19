1ffd28e5d31aef5ff00cc070796aff5cb69470fd
NikkiMerena <alderman.nikki1022@gmail.com>
2023-04-18 23:24:16 -0500

    comments

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+
+""" Starts a Flask Web Application """
+
+# Import necessary modules and classes from the
+# models file, as well as other modules
 from models import storage
 from models.state import State
 from models.city import City
@@ -8,40 +12,39 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
+
+# Initialize the Flask app
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
-
+# Define a function to remove the current SQLAlchemy
+# session when the app context is torn down
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
     storage.close()
 
-
+# Define a route for the homepage of the app
 @app.route('/1-hbnb', strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
+    # Retrieve data from the database and sort it as needed
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
-
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
-
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
+    # Render the homepage template with the retrieved data
+    # and a cache ID generated using uuid.uuid4()
     return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
                            cache_id=uuid.uuid4())
 
-
+# Start the app if this script is being run directly, with
+# debugging enabled and on all interfaces
 if __name__ == "__main__":
-    """ Main Function """
     app.run(debug=True, host='0.0.0.0', port=5000)
